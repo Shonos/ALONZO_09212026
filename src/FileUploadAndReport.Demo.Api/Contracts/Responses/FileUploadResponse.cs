2bed1ca843a84ea9c47bfef26cdfe934aa6ee76e
Shaun Alonzo <shaunalonzodev@gmail.com>
2026-09-21 20:29:08 +0800

    fix: default counter value to 1

--- src/FileUploadAndReport.Demo.Api/Contracts/Responses/FileUploadResponse.cs
+++ src/FileUploadAndReport.Demo.Api/Contracts/Responses/FileUploadResponse.cs
@@ -7,6 +7,6 @@ public sealed class FileUploadResponse
     [DefaultValue("Sample message")]
     public string Message { get; init; } = "Sample message";
 
-    [DefaultValue(10)]
-    public int FilesUploadedCounter { get; init; } = 10;
+    [DefaultValue(1)]
+    public int FilesUploadedCounter { get; init; } = 1;
 }
